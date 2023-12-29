82435faa43f127e117ef89b627ebb2f7f17dbc81
Jaewon Kim <epiloum@gmail.com>
2023-12-29 18:08:47 +0900

    3-2 CRUD API with Database / Getter & Setter by Lombok

--- spring/src/main/java/net/epiloum/eats/shop/Shop.java
+++ spring/src/main/java/net/epiloum/eats/shop/Shop.java
@@ -2,9 +2,12 @@ package net.epiloum.eats;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import lombok.*;
 
 @Entity
 @Table(name = "shops")
+@Getter
+@Setter
 public class Shop {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,9 +20,11 @@ public class Shop {
     private String location;
 
     @Column(name = "created_at", nullable = false, updatable = false)
+    @Setter(AccessLevel.NONE)
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at")
+    @Setter(AccessLevel.NONE)
     private LocalDateTime updatedAt;
 
     @Column(name = "deleted_at")
