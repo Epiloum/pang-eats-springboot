f8e6a3ed180aafb0961320b2c0e468ce579747ca
Jaewon Kim <epiloum@gmail.com>
2023-12-30 20:44:27 +0900

    4-3 API with Relation between Entities / CRUD Operation 작성

---
+++ spring/src/main/java/net/epiloum/eats/dish/DishRepository.java
@@ -0,0 +1,8 @@
+package net.epiloum.eats;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DishRepository extends JpaRepository<Dish, Long> {
+}
